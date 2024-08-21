bd6598244196370dc41b61f65ea0526737c89700
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-21 16:33:36 +0300

    set visibility to true

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -6,6 +6,7 @@ public class MyFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setLayout(null);
+        this.setVisible(true);
 
     }
 }
