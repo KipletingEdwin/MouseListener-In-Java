3a1bccc4d5468a4c0fb6674f28df4b012b382462
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-21 16:32:43 +0300

    setLayout to null

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -5,6 +5,7 @@ public class MyFrame extends JFrame {
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
+        this.setLayout(null);
 
     }
 }
