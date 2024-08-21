1d5324b7ab17d64bd19eda4c36a4a2330822761e
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-21 16:48:40 +0300

    instantiate a label

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,36 +4,47 @@ import java.awt.event.MouseListener;
 
 public class MyFrame extends JFrame implements MouseListener {
 
+    JLabel label;
+
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setLayout(null);
+
+        label = new JLabel();
+
+
         this.setVisible(true);
 
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
+        //Invoked when the mouse has been clicked (pressed and released) on a component
 
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
+        //Invoked when a mouse button has been pressed on a component
 
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+        //Invoked when a mouse button has been released on a component
 
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
+        //Invoked when the mouse enters a component
 
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
+        //Invoked when the mouse exits a component
 
     }
 }
